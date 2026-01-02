7a5137185185f122fcd60f36b4d3d32e145bfbad
lxdcumt <1141051934@qq.com>
2026-01-02 21:17:32 +0800

    polish

--- megatron/training/arguments_fs.py
+++ megatron/training/arguments_fs.py
@@ -13,7 +13,7 @@ except:
     warnings.warn(
         "flagcx is not installed, you can't use flagcx backend for communication.", ImportWarning
     )
-
+from plugin.hetero.parallel_context import RankMapper
 
 class FSTrainArguments:
     """Extend the Megatron arguments with FlagScale specific arguments."""
@@ -72,7 +72,6 @@ class FSTrainArguments:
 
     def _build_rank_mapper(self):
         self._initialize_distributed()
-        from plugin.hetero.parallel_context import RankMapper
         self._rank_mapper = RankMapper(self.args)
         return self._rank_mapper
 
