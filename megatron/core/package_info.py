78c983e98c8eed98024e5272e633326047ab121e
liyuzhuo <lee.yuzhuo233@gmail.com>
2026-03-25 19:43:33 +0800

    v0.1.0+megatron0.15.0rc7

--- megatron/core/package_info.py
+++ megatron/core/package_info.py
@@ -2,15 +2,16 @@
 
 
 MAJOR = 0
-MINOR = 16
+MINOR = 1
 PATCH = 0
-PRE_RELEASE = 'rc0'
+PRE_RELEASE = ''
+LOCAL = 'megatron0.15.0rc7'
 
 # Use the following formatting: (major, minor, patch, pre-release)
 VERSION = (MAJOR, MINOR, PATCH, PRE_RELEASE)
 
 __shortversion__ = '.'.join(map(str, VERSION[:3]))
-__version__ = '.'.join(map(str, VERSION[:3])) + ''.join(VERSION[3:])
+__version__ = '.'.join(map(str, VERSION[:3])) + ''.join(VERSION[3:]) + (f'+{LOCAL}' if LOCAL else '')
 
 __package_name__ = 'megatron_core'
 __contact_names__ = 'NVIDIA'
